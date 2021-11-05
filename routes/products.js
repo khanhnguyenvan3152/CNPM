acae67f28cc395ad98ff36b15bde330ac88a36c7
khanhnguyenvan3152 <54436766+khanhnguyenvan3152@users.noreply.github.com>
2021-11-05 19:51:20 +0700

    fill slider with real product data

--- routes/products.js
+++ routes/products.js
@@ -14,5 +14,15 @@ router.get('/:productCode',async (req,res,next)=>{
     }
     
 })
-
+router.get('/getAllProductByType/:type',async (req,res)=>{
+    let type = req.params.type;
+    try{
+        let product = await Product.find({type:type}).limit(5).exec();
+        res.json({product:product});
+    }
+    catch(err){
+        console.log(err);
+        res.send(err);
+    }
+})
 module.exports = router;
