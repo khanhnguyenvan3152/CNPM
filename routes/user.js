ea33913ee806cab2469c1469bbf9b45bdaef2b17
khanhnguyenvan3152 <54436766+khanhnguyenvan3152@users.noreply.github.com>
2021-11-27 04:34:30 +0700

    orders page

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var Outbill = require('../models/outbill')
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,7 +12,11 @@ router.get('/info', function(req, res, next) {
   let user = req.user;
   res.render('profile',{user:user})
 });
-router.get('/orders',function(req,res){
-  res.render('order')
+router.get('/orders',async function(req,res){
+  let userEmail = req.user.email;
+  console.log(userEmail)
+  let orders =await Outbill.find({"user.email":userEmail}).exec();
+  console.log(orders);
+  res.render('order',{orders:orders})
 })
 module.exports = router;
